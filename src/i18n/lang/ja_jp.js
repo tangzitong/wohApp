bbb0725587122128d11a59f9ed5af0e69ff8f60a
海威 <tei952@hotmail.com>
2019-02-02 11:38:57 +0900

    コッミト２

---
+++ src/i18n/lang/ja_jp.js
@@ -0,0 +1,58 @@
+export default {
+  app: {
+    app_name: 'wohApp',
+    back: '戻る',
+    save: '保存',
+    unknown: '未知',
+    home: 'トップ',
+    contacts: '連絡者',
+    settings: '設定',
+    city: '都市',
+    profile: '個人情報',
+    language: '言語選択',
+    feedback: 'アドバイス',
+    about: 'アバウトアス',
+    avatar: 'アバター',
+    name: 'ニックネーム',
+    gender: '性别',
+    location: '位置',
+    male: '男',
+    female: '女',
+    send: '送信',
+    cancel: '取消',
+    chat: 'チャット',
+    close: '閉じる',
+    submitting: 'コッミト中...',
+    empty_container: 'コンテンツなし~',
+    modal: {
+      title: 'システム開発タイトル',
+      button_ok: '確定'
+    }
+  },
+  home: {
+    comment: 'コメント',
+    like: '喜欢',
+    noNewestPost: 'ポストなし'
+  },
+  contacts: {
+    placeholder: '連絡者検査',
+    empty: '連絡者なし'
+  },
+  feedback: {
+    placeholder: 'Hi, ご意見がありますか?',
+    result: 'ご意見ありがとうございます。。'
+  },
+  message: {
+    placeholder: 'メッセージください...'
+  },
+  comment: {
+    placeholder: 'コメントください...'
+  },
+  publisher: {
+    publisher: '配布者',
+    placeholder: '最新情報を共有しましょう...'
+  },
+  post: {
+    post: '文章'
+  }
+}
