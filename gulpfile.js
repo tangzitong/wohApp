3eadb267d2d4159418c4fe84ad6e8dbae16ba470
海威 <tei952@hotmail.com>
2019-03-08 19:55:53 +0900

    nothing

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var runTimestamp = Math.round(Date.now() / 1000);
+var async = require('async');
+var consolidate = require('gulp-consolidate');
 
-gulp.task('Iconfont', function () {
+gulp.task('Iconfont_f', function () {
   return gulp.src(['src/assets/fonts/*.svg'])
     .pipe(iconfont({
-      fontName: 'myfont', // required
+      fontName: 'iconfont', // required
       prependUnicode: true, // recommended option
-      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      formats: ['ttf', 'eot'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
     .on('glyphs', function (glyphs, options) {
@@ -15,4 +17,30 @@ gulp.task('Iconfont', function () {
       console.log(glyphs, options);
     })
     .pipe(gulp.dest('src/assets/fonts/'));
+});
+
+gulp.task('Iconfont_c', function (done) {
+  var iconStream = gulp.src(['src/assets/icons/*.svg'])
+    .pipe(iconfont({ fontName: 'iconfont' }));
+
+  async.parallel([
+    function handleGlyphs(cb) {
+      iconStream.on('glyphs', function (glyphs, options) {
+        gulp.src('src/assets/fonts/iconfont.css')
+          .pipe(consolidate('lodash', {
+            glyphs: glyphs,
+            fontName: 'iconfont',
+            fontPath: 'src/assets/fonts/',
+            className: 's'
+          }))
+          .pipe(gulp.dest('src/assets/fonts/'))
+          .on('finish', cb);
+      });
+    },
+    function handleFonts(cb) {
+      iconStream
+        .pipe(gulp.dest('src/assets/fonts/'))
+        .on('finish', cb);
+    }
+  ], done);
 });
