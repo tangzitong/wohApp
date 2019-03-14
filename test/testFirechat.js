b3d83113cca5488638a1014d0462d20ee77025cb
海威 <tei952@hotmail.com>
2019-03-14 13:17:58 +0900

    Update testFirechat.js

--- test/testFirechat.js
+++ test/testFirechat.js
@@ -1,7 +1,38 @@
-const fb = require('../src/firebaseConfig.js')
+import * as fb from '../src/firebaseConfig.js'
 
-exports['getPostList'] = function(test) {
+fb.auth.createUserWithEmailAndPassword('test1@gmail.com', '12345qwert').then(user => {
+  // create user obj
+  fb.database.child('users').push({
+    login_name: 'test1@gmail.com',
+    nick_name: 'test1',
+    points: 0,
+    avatar_url: '',
+    gender: '',
+    location: '',
+    invites: [],
+    muted: [],
+    rooms: []
+  })
+})
+
+fb.auth.onAuthStateChanged(user => {
+  if (user) {
+    fb.database.child('users').orderByChild('login_name').equalTo(user.uid).once('value', function(snapshot){
+      fb.chat.setUser(snapshot.key, snapshot.val().nick_name, null)
+    })        
+  }
+})
+
+fb.auth.signInWithEmailAndPassword('test1@gmail.com', '12345qwert')
+
+exports['createPost'] = function(test) {  
+  const posts = fb.chat.createPost()
+  test.equal(posts.count, 0)
+  test.done()
+}
+
+exports['getPostList'] = function(test) {  
   const posts = fb.chat.getPostList()
-  test.equal(posts.count, 1)
+  test.equal(posts.count, 0)
   test.done()
 }
