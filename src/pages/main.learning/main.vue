2be3d8d5b6118dd71a7184c85b2cdf9d1f029946
海威 <tei952@hotmail.com>
2019-06-27 15:54:52 +0900

    Update main.vue

--- src/pages/main.learning/main.vue
+++ src/pages/main.learning/main.vue
@@ -1,6 +1,9 @@
 <template>
   <f7-page>
     <f7-navbar>
+      <f7-nav-left>
+        <f7-link icon="iconfont icon-login" icon-size="22" v-show="activedTab === 'home'" @click="openDownload"></f7-link>
+      </f7-nav-left>
       <f7-nav-title>{{navbarTitle}}</f7-nav-title>
       <f7-nav-right>
         <f7-link icon="iconfont icon-feedback3" icon-size="22" v-show="activedTab === 'news'" @click="openPublisher"></f7-link>
@@ -179,6 +182,9 @@ export default {
         key: 'loginOpened',
         value: true
       })
+    },
+    openDownload() {
+      this.$f7router.navigate(`/download`)
     }
   },
   components: {
