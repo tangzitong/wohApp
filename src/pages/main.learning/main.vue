e0f7ac87247a2bbce2b9bd94a6a96534f4978103
海威 <tei952@hotmail.com>
2019-06-27 15:55:34 +0900

    Update main.vue

--- src/pages/main.learning/main.vue
+++ src/pages/main.learning/main.vue
@@ -2,7 +2,7 @@
   <f7-page>
     <f7-navbar>
       <f7-nav-left>
-        <f7-link icon="iconfont icon-login" icon-size="22" v-show="activedTab === 'home'" @click="openDownload"></f7-link>
+        <f7-link icon="iconfont icon-download" icon-size="22" v-show="activedTab === 'home'" @click="openDownload"></f7-link>
       </f7-nav-left>
       <f7-nav-title>{{navbarTitle}}</f7-nav-title>
       <f7-nav-right>
